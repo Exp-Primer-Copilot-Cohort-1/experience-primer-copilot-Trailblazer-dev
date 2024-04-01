08aa0c6fcdcbc074f000c8a2094b808a74b8aa8e
Trailblazer <124080190+Trailblazer-dev@users.noreply.github.com>
2024-04-01 14:00:36 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember(){
+    var skills = ['JavaScript', 'React', 'Node', 'CSS', 'HTML'];
+    var member = {
+        name: 'Sally',
+        age: 35,
+        skills: skills
+    };
+    return member;
+}
